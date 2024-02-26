66c6d8d237485f08ae67434001aa23c9cb4ad986
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-02-26 18:25:42 +0530

    docs: readme updated, utility functions updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# roza
+## Exnode
+
+[![npm version](https://badge.fury.io/js/exnode.svg)](https://www.npmjs.com/package/exnode)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+> ⚡Exnode is a modern Node.js framework designed for ease of use and flexibility in building server-side applications. With its intuitive architecture and powerful features, Exnode empowers developers to create scalable and efficient web applications with minimal boilerplate.
+
+### Installation
+
+```bash
+npm install exnode
+```
--- src/utils/rainbow_text.ts
+++ src/utils/rainbow_text.ts
@@ -1,10 +1,23 @@
 function rainbow_text(text: string) {
-    const colors = ['\x1b[1;31m', '\x1b[1;33m', '\x1b[1;32m', '\x1b[1;36m', '\x1b[1;34m', '\x1b[1;35m'];
+
+    const rainbowColors = [
+        { r: 255, g: 0, b: 0 },     // Red
+        { r: 255, g: 127, b: 0 },   // Orange
+        { r: 255, g: 255, b: 0 },   // Yellow
+        { r: 0, g: 255, b: 0 },     // Green
+        { r: 0, g: 0, b: 255 },     // Blue
+        { r: 75, g: 0, b: 130 },    // Indigo
+        { r: 148, g: 0, b: 211 }    // Violet
+    ];
+
     let rainbowText = '';
     for (let i = 0; i < text.length; i++) {
-        rainbowText += `${colors[i % colors.length]}${text[i]}`;
+        const colorIndex = Math.round(i / text.length * (rainbowColors.length - 1));
+        const color = rainbowColors[colorIndex];
+        rainbowText += `\x1b[38;2;${color.r};${color.g};${color.b}m${text[i]}`;
     }
     return rainbowText + '\x1b[0m';
+
 };
 
 export { rainbow_text };
